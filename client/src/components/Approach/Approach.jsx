84d7d7d723ff8a471cf3df335245e8d91af68319
sbaccon <sbaccon.absti@gmail.com>
2023-04-04 00:00:00 -0300

    cambios visuales

--- client/src/components/Approach/Approach.jsx
+++ client/src/components/Approach/Approach.jsx
@@ -7,8 +7,16 @@ const Approach = () => {
     <div id="approach" className="approach">
       <h2>We develop things<br />to bring your ideas to life</h2>
       {/* <img src={CallstackIcon} alt="" /> */}
-      <div className="data-approach">
-        <p>Landing page - Launch page - E-Commerce</p>
+      <div className="data-container">
+        {/* <div className="data-approach">
+          <p><span>Landing page</span> - <span>Launch page</span> - <span>E-Commerce</span></p>
+        </div>
+        <div className="data-approach">
+          <p><span>Landing page</span> - <span>Launch page</span> - <span>E-Commerce</span></p>
+        </div> */}
+        <p className="data-one">Landing Page</p>
+        <p className="data-two">Launch Page</p>
+        <p className="data-three">E-Commerce</p>
       </div>
     </div>
   )
